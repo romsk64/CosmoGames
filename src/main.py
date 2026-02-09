919d5b073dce2014a1b66184f951519eff9da4b3
romsk64 <roma08shm@ya.ru>
2026-02-09 18:46:31 +0300

    main added

---
+++ src/main.py
@@ -0,0 +1,20 @@
+# romsk64, 2026
+
+import pygame
+import json
+
+pygame.init()
+bg = pygame.display.set_mode((1920, 1000))
+_menu_ = True
+
+def startlog():
+    open_col = open("system.txt")
+def log(level: int, message: str, method: str):
+    print(f"[{level}: {method}]: {message}")
+
+def menu(bg):
+    pass
+def settings(bg):
+    pass
+def main(bg):
+    pass
